a04b36b9d95a3698b92f43ff33b855bc2dc23a5f
Takuma <qwerty@example.com>
2016-04-11 19:50:36 +0900

    block rotaion

--- ar2016/block_object.h
+++ ar2016/block_object.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "object.h"
+#define _USE_MATH_DEFINES // for C++
+#include <math.h>
 
 class BlockObject : public Object {
 	bool isStay;
@@ -20,8 +22,25 @@ public:
 	bool draw() {
 		if (isStay) {
 			DrawBox(left(), top(), right(), bottom(), GetColor(240, 117, 28), false);
+				
+			
 		} else {
-			DrawExtendGraph(left(), top(), right(), bottom(), imgHandles["block"], true);
+			double reti = (right() - left()) / 50.0;
+			switch (rect.direction) {
+				case 0:
+
+					DrawRotaGraph(rect.x + rect.width / 2, rect.y + rect.height / 2, reti, 0, imgHandles["block"], true);
+					break;
+				case 1:
+					DrawRotaGraph(rect.x + rect.width / 2, rect.y + rect.height / 2, reti, M_PI_2, imgHandles["block"], true);
+					break;
+				case 2:
+					DrawRotaGraph(rect.x + rect.width / 2, rect.y + rect.height / 2, reti, M_PI, imgHandles["block"], true);
+					break;
+				case 3:
+					DrawRotaGraph(rect.x + rect.width / 2, rect.y + rect.height / 2, reti, -1*M_PI_2, imgHandles["block"], true);
+					break;
+			}
 		}
 		return isStay;
 	}
