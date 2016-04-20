f7f4acb681cfc28254348a1a4ec02ba183bb4601
Takahashi Ryo <ryo.t.5974@gmail.com>
2016-04-20 18:40:19 +0100

    Revert "途中まで"
    This reverts commit c12c2ad9e20e4df477d50ec0250e60a8f9825f41.

--- ar2016/src/object/object.cpp
+++ ar2016/src/object/object.cpp
@@ -56,7 +56,6 @@ bool Object::load() {
 
 	isSuccess &= loadImage("player", "mario");
 	isSuccess &= loadImage("block", "block");
-	isSuccess &= loadImage("brakout-background", "firefox");
 
 	//メニューで使う
 	isSuccess &= loadImage("monhan", "monhan");
