15396777e0732866ed8b5bd7aa06158f74b60b69
Takahashi Ryo <ryo.t.5974@gmail.com>
2016-04-22 07:29:30 +0100

    objectの位置を画面中心に置いた

--- ar2016/include/game/breakout_components.h
+++ ar2016/include/game/breakout_components.h
@@ -10,6 +10,8 @@
 #include <Eigen/Core>
 
 // Blockのパラメータ
+const int BLOCK_OFFSET_X = WIDTH / 3;
+const int BLOCK_OFFSET_Y = 10;
 constexpr int BLOCK_WIDTH_NUM = 10;
 constexpr int BLOCK_HEIGHT_NUM = 5;
 constexpr int BLOCK_WIDTH= 50;
@@ -21,8 +23,8 @@ const Eigen::Vector2i FIREBALL_STRATPOS = Eigen::Vector2i{
 constexpr int FIREBALL_RADIUS = 30;
 
 // Shipのパラメータ
-const Eigen::Vector2i SHIP_START_POS = Eigen::Vector2i{WIDTH / 2, HEIGHT - 200};
-constexpr int SHIP_WIDTH = 100;
+const Eigen::Vector2i SHIP_START_POS = Eigen::Vector2i{BLOCK_OFFSET_X, HEIGHT - 100};
+constexpr int SHIP_WIDTH = 200;
 constexpr int SHIP_HEIGHT = 50;
 
 class BreakoutComponents
@@ -34,7 +36,8 @@ public:
 	{
 		for (int x = 0; x < BLOCK_WIDTH_NUM; ++x) {
 			for (int y = 0; y < BLOCK_HEIGHT_NUM; ++y) {
-				const Eigen::Vector2i realm = Eigen::Vector2i{ x * BLOCK_WIDTH, y * BLOCK_HEIGHT };
+				const Eigen::Vector2i realm 
+					= Eigen::Vector2i{BLOCK_OFFSET_X + x * BLOCK_WIDTH, BLOCK_OFFSET_Y + y * BLOCK_HEIGHT };
 			
 				const Shape::Rectangle rec(realm,BLOCK_WIDTH, BLOCK_HEIGHT);
 
@@ -50,7 +53,7 @@ public:
 
 		
         const Shape::Rectangle ship_realm
-			= Shape::Rectangle(Eigen::Vector2i::Zero(), 100, 100);
+			= Shape::Rectangle(SHIP_START_POS, SHIP_WIDTH, SHIP_HEIGHT);
 
         ship = std::make_shared<Breakout::Ship>(ship_realm);
 		// Todo Player
