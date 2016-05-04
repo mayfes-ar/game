28eb53c2dc448a92457d205270ccf24c2fe2c39a
Takuma <qwerty@example.com>
2016-05-04 15:30:01 +0900

    no message

--- ar2016/src/game/puzzle_game.cpp
+++ ar2016/src/game/puzzle_game.cpp
@@ -15,7 +15,7 @@ bool PuzzleGame::onStart() {
 		setPlayer(100, 600);
 		setGoal(1100, 550);
 	}, -1);
-
+	
 	// mode 1
 	mode.setMode([this]() {
 		makeStageBase();
@@ -84,7 +84,7 @@ bool PuzzleGame::onStart() {
 		setCoin(900, 370);
 		setCoin(980, 370);
 
-		setBlock(0, 680, 1300, 200);
+		//setBlock(0, 680, 1300, 200);
 
 	}, -1);
 
@@ -106,12 +106,12 @@ bool PuzzleGame::onStart() {
 		setCoin(770, 550);
 
 	}, -1);
-
+	
 	// mode 5
 	mode.setMode([this]() {
 		makeStageBase(false);
 		setPlayer(100, 90);
-		setGoal(1000, 500);
+		setGoal(950, 500);
 
 		for (int i = 0; i * 50 <= 1200; i++) {
 			setLoop(i * 50, 780, 50, 10, (i - 1) * 50, 0);
@@ -139,8 +139,8 @@ bool PuzzleGame::onStart() {
 		setBlock(700, 400, 24, 240);
 		setBlock(464, 400, 236, 24);
 		setBlock(464, 616, 236, 24);
-		setWarp(680, 424, 20, 20, 600, 300);
-		setWarp(1250, 680, 20, 20, 520, 500);
+		setWarp(620, 424, 60, 60, 600, 300);
+		setWarp(1200, 650, 60, 60, 520, 500);
 		setCoin(40, 240);
 		setCoin(120, 240);
 		setCoin(200, 240);
