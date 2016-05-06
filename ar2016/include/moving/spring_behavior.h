df02060622b7f2a478416969c15171a354ea5765
Nisho Matsushita <nichyou@gmail.com>
2016-05-07 02:25:28 +0900

    Fix spring_behavior

--- ar2016/include/moving/spring_behavior.h
+++ ar2016/include/moving/spring_behavior.h
@@ -13,9 +13,12 @@ public:
 	void update(float time_step, Eigen::Vector2f& pos, Eigen::Vector2f& vel, Eigen::Vector2f& accel) override
 	{
 		static constexpr float pi = 3.141592f;
-		static const Eigen::Vector2f pos_origin = pos;
+		if (!m_is_origin_pos_init) {
+			m_origin_pos = Eigen::Vector2f(pos);
+			m_is_origin_pos_init = true;
+		}
 		const float omega = 2.0f * pi * m_freq;
-		pos = pos_origin  + m_amp * std::sin(omega * m_time_cnt);
+		pos = m_origin_pos + m_amp * std::sin(omega * m_time_cnt);
 		vel = m_amp * omega * std::cos(omega * m_time_cnt);
 		accel = -m_amp * std::pow(omega, 2.0f) * std::sin(omega * m_time_cnt);
 		m_time_cnt += time_step;
@@ -25,4 +28,6 @@ private:
 	float m_time_cnt = 0.0f;
 	Eigen::Vector2f m_amp = Eigen::Vector2f::Zero();
 	float m_freq = 0.0f;
+	bool m_is_origin_pos_init = false;
+	Eigen::Vector2f m_origin_pos;
 };
