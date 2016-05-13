62932bfa8fff916eb1e2d4204fc92b48effc5ed1
gaku kotani <manabukk618@gmail.com>
2016-05-13 19:28:10 +0900

    メニューの基本

--- ar2016/src/game/menu.cpp
+++ ar2016/src/game/menu.cpp
@@ -24,23 +24,29 @@ std::shared_ptr<Game> startMenu() {
 // メニュー画面に表示するゲーム情報の登録
 Menu::SelectGame::SelectGame() {
 	gameList = std::vector<GameDescription>{
-		GameDescription("バトルネットワーク ロックマンエグゼ3",
-		"大人気のデータアクションRPG「ロックマンエグゼ」シリーズの第3弾。\n主人公の熱斗が現実世界で暴走する電気機器をさがし、\nロックマンが電脳世界でウィルスを退治するというように、\n協力して事件を解決していく。\nロックマンエグゼ！トランスミッション！！",
-			imgHandles["rockman"],
+		GameDescription(
+			imgHandles["p_perfect"],
+			imgHandles["s_game_op"],
+			100,
+			120,
 			150,
-			270,
+			90,
 			[]() -> std::shared_ptr<Game> {return std::make_shared<PuzzleGame>(); }),
-		GameDescription("オタサーの姫ゲーム",
-						"宇宙のどこかの星に美しいお姫様がいて、ずっとかわいがられてきました。\n今、国王様がお姫様の婚約者を全宇宙から募集しようとしています。\n迫りくる試練からお姫様を守り切った人こそ、婚約者にふさわしい。\n勇気あるあなた、チャレンジしてみませんか？",
-						imgHandles["s_game_player_drowned"],
-						621 / 4,
-						1046 / 4,
-						[]() -> std::shared_ptr<Game> {return std::make_shared<SinglePlayerGame>(); }),
-		GameDescription("New スーパーマリオブラザーズ", // ゲームタイトル
-			"「誰でもできる、新しいマリオ。」\n跳んで走って、投げて踏んで、たたいて壊して、探して集める…。\nいろんな動きをカンタンにこなし、マリオがゴールを目指す。\n海や森、氷や砂の世界など、まったく新しいキノコ王国を舞台に、\nマリオの新しい冒険が始まる。", // 説明文
-			imgHandles["player"], // サムネイルのハンドル
+		GameDescription(
+			imgHandles["s_game_cake"],
+			imgHandles["s_game_op"],
+			100,
+			320,
 			150,
-			225, // 起動するゲーム
+			90,
+			[]() -> std::shared_ptr<Game> {return std::make_shared<SinglePlayerGame>(); }),
+		GameDescription(
+			imgHandles["b_donald"], // サムネイルのハンドル
+			imgHandles["s_game_block"],
+			100,
+			520,
+			150,
+			90,
 			[]() -> std::shared_ptr<Game> {return std::make_shared<BreakoutGame>(); })
 	};
 
