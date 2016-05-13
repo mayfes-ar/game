3b2ffb82e9575a8c194d0a40d37b47374fbfb40e
gaku kotani <manabukk618@gmail.com>
2016-05-14 05:28:52 +0900

    バグ修正

--- ar2016/src/game/single_game_player.cpp
+++ ar2016/src/game/single_game_player.cpp
@@ -131,11 +131,6 @@ std::shared_ptr<SinglePlayerGame::tutoFire> SinglePlayerGame::maketutoFire(int x
 }
 
 
-
-bool willFinishMode = false;
-bool isChangingMode = false;
-int counterForWait = 5;
-
 bool SinglePlayerGame::onStart() {
 	using namespace std;
 	fps.isShow = true;
@@ -739,6 +734,7 @@ bool SinglePlayerGame::onUpdate() {
 				difficulty = NIGHTMARE;
 			}
 			else if (key[KEY_INPUT_UP]) {
+				difficulty = difficulty == EASY ? NIGHTMARE : difficulty == HARD ? EASY : HARD;
 			}
 			else if (key[KEY_INPUT_DOWN]) {
 				difficulty = difficulty == EASY ? HARD : difficulty == HARD ? NIGHTMARE : EASY;
@@ -1082,7 +1078,6 @@ bool SinglePlayerGame::onUpdate() {
 	if (key[KEY_INPUT_ESCAPE]) {
 		share.willFinish = true;
 	}
-	static int waitNCounter = FPS / 3;
 	if (key[KEY_INPUT_N] && waitNCounter == 0) {
 		willFinishMode = true;
 		waitNCounter = FPS / 3;
